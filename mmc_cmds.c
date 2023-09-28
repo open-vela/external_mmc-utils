566cd918119d05d2d3a7ba89416df6c570e153f7
wanggang26 <wanggang26@xiaomi.com>
2023-09-28 16:26:47 +0800

    mmc-utils: fix potential overflow
    Building with _FORTIFY_SOURCE=3 results in:
                     from mmc_cmds.c:20:
    In function ‘read’,
        inlined from ‘do_rpmb_write_key’ at mmc_cmds.c:2233:8:
    ..unistd.h:38:10: error: ‘__read_alias’ writing 228 or more bytes into a
    region of size 32 overflows the destination [-Werror=stringop-overflow=]
       38 |   return __glibc_fortify (read, __nbytes, sizeof (char),
          |          ^~~~~~~~~~~~~~~
    mmc_cmds.c: In function ‘do_rpmb_write_key’:
    mmc_cmds.c:2087:19: note: destination object ‘key_mac’ of size 32
     2087 |         u_int8_t  key_mac[32];
          |                   ^~~~~~~
    ..unistd.h:26:16: note: in a call to function ‘__read_alias’ declared
    with attribute ‘access (write_only, 2, 3)’
       26 | extern ssize_t __REDIRECT (__read_alias, (int __fd, void *__buf,
          |                ^~~~~~~~~~
    
    To work around this let's check if the return of read() is lower than
    the
    nbyte requested instead of not equal.
    
    Signed-off-by: wanggang26 <wanggang26@xiaomi.com>

--- mmc_cmds.c
+++ mmc_cmds.c
@@ -2072,7 +2072,7 @@ int do_sanitize(int nargs, char **argv)
 			}										\
 			else if (r > 0)							\
 				ret += r;							\
-		} while (r != 0 && (size_t)ret != nbyte);	\
+		} while (r != 0 && (size_t)ret < nbyte);	\
 													\
 		ret;										\
 	})
